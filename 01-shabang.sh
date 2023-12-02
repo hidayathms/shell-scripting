976b1ad4318831f25d9f21ffcb059065acbab2a8
Hidayath <hidayath@aol.com>
2023-12-02 09:59:01 +0530

    AutoSave

--- 01-shabang.sh
+++ 01-shabang.sh
@@ -3,8 +3,12 @@
 echo Welcome to Shell Scripting
 echo "This is the first test of shell scripting"
 
+# Escape sequence characters
+#\n cursor move to new line
+#\t move tab space
+# -e enable escape sequence characters
+
 echo "Line1\nLine2"
-echo Line3
-echo Line4
+echo "Line3\t\Line4"
 echo Line5
 echo Line6
