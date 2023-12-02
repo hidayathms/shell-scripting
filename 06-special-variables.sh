f88acd7aadd211622610c339abd8fd5f2e37ff95
Hidayath <hidayath@aol.com>
2023-12-02 11:02:04 +0530

    AutoSave

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -3,6 +3,6 @@
 # Special variables are $0 to $9,$*, $@, $#, $$
 
 echo Name of the script name :  $0
-echo First argument $1
-echo Second argument $2
-echo fifth argument $5
+echo First argument : $1
+echo Second argument : $2
+echo fifth argument : $5
