7a881ff3b571de5c1c0d6000dd7eba08dbfe4b30
Hidayath <hidayath@aol.com>
2023-12-02 10:50:38 +0530

    AutoSave

--- 05-dynamic-var.sh
+++ 05-dynamic-var.sh
@@ -3,6 +3,6 @@
 # DATE="01DEC2023"
 DATE=$(date +%F)
 NO_0F_SESSIONS=$(who | wc -l)
-echo Good Morning, Todays data is $DATE
+echo -e Good Morning, Todays data is \e[34m $DATE \e[0m
 echo No os sessions is $NO_0F_SESSIONS
 
