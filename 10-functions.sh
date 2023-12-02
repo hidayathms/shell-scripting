0327c8c4ae3defdbc09759860e0391629f9b1856
Hidayath <hidayath@aol.com>
2023-12-02 11:58:27 +0530

    AutoSave

--- 10-functions.sh
+++ 10-functions.sh
@@ -27,7 +27,7 @@ b56(){
 stat(){
     echo " Number of sessions opened $(who|wc -l)"
     echo "Todays date is $(date +%F)" 
-    echo "Avg cpu utilization $(uptime|awk -F '{print $NF}' | awk -F ',' '{Print $2}')"
+    echo "Avg cpu utilization $(uptime|awk -F : '{print $NF}' | awk -F ',' '{print $2}')"
 }
 
 stat
