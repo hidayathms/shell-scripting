df256d12f79210d063d35699f161df8e1966557c
Hidayath <hidayath@aol.com>
2023-12-02 09:55:48 +0530

    AutoSave

--- 01-shabang.sh
+++ 01-shabang.sh
@@ -3,9 +3,8 @@
 echo Welcome to Shell Scripting
 echo "This is the first test of shell scripting"
 
-echo Line1
-echo Line2
+echo "Line1\nLine2"
 echo Line3
 echo Line4
 echo Line5
-echo Line 6
+echo Line6
