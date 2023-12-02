d12096d139ed430e1696310a38c3d14a280bb6ec
Hidayath <hidayath@aol.com>
2023-12-02 10:26:55 +0530

    AutoSave

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# What is a variable 
+# A variable is somthing that holds data dynamically
+a=100
+b=abc
+# The advantage and disadvantate in shell is that there are no Data Types
+# In bash, everything iwill be considered as string by default.
+# How can we print a variable ?$var
+
+echo pinting the value of $a
+echo pinting the value of $(b)
