c748a0c4be9f11feb140878e2206bb252cb70654
Hidayath <hidayath@aol.com>
2023-12-08 20:51:43 +0530

    AutoSave

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -10,7 +10,9 @@ fi
 echo -e "********* \e[35m Configuring frontend  \e[0m ************"
 
 echo -e " Installing NGINX"
-yum install nginx -y
+yum install nginx -y > /tmp/$1.log
+
+
 # systemctl enable nginx
 # systemctl start nginx
 
