a65bfab9875325adb4fd57895c0f963d269a83a4
Hidayath <hidayath@aol.com>
2023-12-08 10:05:43 +0530

    AutoSave

--- 04-variables.sh
+++ 04-variables.sh
@@ -9,4 +9,4 @@ b=abc
 # How can we print a variable ?$var
 
 echo -e "\e[32m pinting the value of a is $a \e[0"
-echo -e "\e[34m pinting the value of b is $b \e[0"
+echo -e "\e[34m pinting the value of b is b \e[0"
