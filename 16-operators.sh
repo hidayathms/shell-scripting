c68832da92927fc8f01f98247762cd37cd54af3b
Hidayath <hidayath@aol.com>
2023-12-08 19:38:21 +0530

    AutoSave

---
+++ 16-operators.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+<<COMMENT
+# Epressions are catetorized in to three types
+1. Numbers
+2. Strings
+3. Files
+
+Operators on numbers :
+-eq ,  -ne , -gt, -ge, -lt, -le
+
+[1 -eq 1]
+[1 -ne1 ]
+
+Strign operators
+=, ==, !=
+[abc = abc ]
+
+-z , -n  inverse proportional options
+
+[-z "$var" ] this is true if var is not having any data
+[-n "$var" ] this is true if var is  having any data
+
+file operators
+[-f file ] True of file exisists and file is a regualr file
+
+[-d file ] True of file exisists and it is a directory
+
+COMMENT 
+
