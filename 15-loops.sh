4ed7a98e44fb5c5376657e772daee71193757f29
Hidayath <hidayath@aol.com>
2023-12-08 11:41:50 +0530

    AutoSave

---
+++ 15-loops.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Loops : When you wnat to run an action for centain number of times, we use loops
+
+#There are 2 major types of loops:
+ #   1} for loop - when you know something to be executed n number of time, we use for loop
+  #  2) while loop -when you dont know something to be executed n number of times, we use while loop
+
+  echo value is 10
+  echo value is 20
+  echo value is 30
+  echo value is 40
+  echo value is 50
+
