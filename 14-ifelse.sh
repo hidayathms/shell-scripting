41ed8e9851b50cf89383ba7b8d94e0ae367af624
Hidayath <hidayath@aol.com>
2023-12-08 11:06:59 +0530

    AutoSave

--- 14-ifelse.sh
+++ 14-ifelse.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo -e "Demo on IF, IF ELSE & ELSE IF usage"
-ACTION = $1
+ACTION=$1
 if [ $ACTION == start ] ; then 
-echo -e "\e[34mStarting shipping service \e[0m]"
+echo -e "\e[34m Starting shipping service \e[0m]"
 exit 0
 fi 
