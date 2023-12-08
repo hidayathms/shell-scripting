3a3911c567fd13320c0f52a9ac40ff5fe9ead748
Hidayath <hidayath@aol.com>
2023-12-08 11:44:22 +0530

    AutoSave

--- 15-loops.sh
+++ 15-loops.sh
@@ -6,9 +6,13 @@
  #   1} for loop - when you know something to be executed n number of time, we use for loop
   #  2) while loop -when you dont know something to be executed n number of times, we use while loop
 
-  echo value is 10
-  echo value is 20
-  echo value is 30
-  echo value is 40
-  echo value is 50
+ # echo value is 10
+  #echo value is 20
+  #echo value is 30
+  #echo value is 40
+  #echo value is 50
 
+#for loop sysntax
+for i in 10 20 30 40 50 ; do 
+echo "Values form the loops are $i"
+done
