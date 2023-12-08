d0428644f12a4425c8cfc2bf321b0df506b765da
Hidayath <hidayath@aol.com>
2023-12-08 11:30:36 +0530

    AutoSave

--- 14-ifelse.sh
+++ 14-ifelse.sh
@@ -8,11 +8,11 @@ if [ "$ACTION" == "start" ] ; then
 echo -e "\e[34m Starting shipping service \e[0m]"
 exit 0
 elif [ "$ACTION" == "stop" ] ; then 
-echo -e "\e[34m stopping shipping service \e[0m]"
-exit 0
-else 
-echo -e "\e[34m Starting shipping service \e[0m]"
+echo -e "\e[35m stopping shipping service \e[0m]"
 exit 1
+else 
+echo -e "\e[36m Starting shipping service \e[0m]"
+exit 2
 fi 
 
 echo "It has not met any conditions"
