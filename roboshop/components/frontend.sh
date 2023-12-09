866538f1303ff9ef0231bd6a3f380ac4afb359eb
Hidayath <hidayath@aol.com>
2023-12-09 19:33:41 +0530

    AutoSave

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -34,7 +34,7 @@ rm -rf *    &>> $LOGFILE
 stat $?
 
 echo -n " Extracting the $COMPONENT : "
-unzip /tmp/$COMPONENT.zip  &>> $LOGFILE
+unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE
 stat $?
 
 echo -n "Configuring $COMPONENT : "
