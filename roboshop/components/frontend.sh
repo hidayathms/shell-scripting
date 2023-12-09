b490ed185fd04d2e224efb46092c8f6a8a7181ec
Hidayath <hidayath@aol.com>
2023-12-09 18:47:55 +0530

    AutoSave

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -23,9 +23,9 @@ echo -n " Downloading the component $1 : "
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 if [ $? -eq 0 ] ; then
-echo -e "\e[32m Success \e[0"
+echo -e "\e[32m Success \e[0m"
 else
-echo -e "\e[34m Failure \e[0"
+echo -e "\e[34m Failure \e[0m"
 fi
 
 # systemctl enable nginx
