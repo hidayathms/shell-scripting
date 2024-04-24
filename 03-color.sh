4a01e1e40bbdc0894754f78df4d8aff0172a0cf7
Hidayath <hidayath@aol.com>
2024-04-24 21:09:11 +0530

    update

--- 03-color.sh
+++ 03-color.sh
@@ -5,9 +5,11 @@
 echo -e "\e[31m I am printing Red color \e[0m"
 echo -e "\e[32m I am printing Green color \e[0m"
 echo -e "\e[33m I am printing Yellow color \e[0m"
-echo -e "\e[34m I am printing red color \e[0m"
+echo -e "\e[34m I am printing Blue color \e[0m"
 
 
+###Background + Foreground Color 
 
+echo -e "e\[43;34m I am printing both the colors\e[0m"
 
 
