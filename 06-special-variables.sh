e54ee40968ffc1833520c56ef754e4c36abb6e73
Hidayath <hidayath@aol.com>
2024-04-24 21:43:31 +0530

    update

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -3,3 +3,10 @@
 # Special Varialbles are like $0 to $09, $a, S#, $$ 
 
 echo " Thins the name of scrip name : $0"
+
+echo "First Argument : $1"
+echo "First Argument : $2"
+echo "First Argument : $3"
+
+# bash scriptName.sh firstArg secondArg thirdArg
+#                       $1          $2      $3     upto $9 which can be read from command line
