93a502bd780facfae79bdd35a3463e6a09381247
Hidayath <hidayath@aol.com>
2024-04-24 21:34:54 +0530

    update

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# DATE="21FEB2024"
+# DATE="21FEB2024"  Whenever using experession need to enclose in paranthesis
 DATE=$(date +%F)
-
+NO_OF_SESSIONS=$(who|wc -l)
 echo " Goood Morning, Todays date is $DATE"
+
+echo -e "\e[34m No of sessions is : $NO_OF_SESSIONS \e[0m "
+
+
