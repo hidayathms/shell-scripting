4e2d2fba8a1b01cd40975ed3358613ebb3709da4
Hidayath <hidayath@aol.com>
2024-04-24 21:48:15 +0530

    update

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Special Varialbles are like $0 to $09, $a, S#, $$ 
+# Special Varialbles are like $0 to $09, $a, S#, $?, $$ 
 
 echo " Thins the name of scrip name : $0"
 
@@ -10,3 +10,8 @@ echo "First Argument : $3"
 
 # bash scriptName.sh firstArg secondArg thirdArg
 #                       $1          $2      $3     upto $9 which can be read from command line
+
+
+echo $0  # print script name
+echo $#  # Prints the overall arguments used in the script
+echo $? # This prints the exit code of the last command
