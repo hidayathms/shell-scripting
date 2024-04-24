17d47c6545c5b3e40646fadc050460285ff2541b
Hidayath <hidayath@aol.com>
2024-04-24 21:07:12 +0530

    update

---
+++ 03-color.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Syntax to Print colors is : echo -e "\e[31m I am printing red color \e[0m"
+
+echo -e "\e[31m I am printing Red color \e[0m"
+echo -e "\e[32m I am printing Green color \e[0m"
+echo -e "\e[33m I am printing Yellow color \e[0m"
+echo -e "\e[34m I am printing red color \e[0m"
+
+
+
+
+
