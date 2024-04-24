9a96181f56f373498ab024abdeac3790067fc2d3
Hidayath <hidayath@aol.com>
2024-04-24 20:57:58 +0530

    update

---
+++ 02-esc-sequence.sh
@@ -0,0 +1,10 @@
+#!/bin/shell
+
+#Anything that starts with # is referred as a comment.
+# Comments are just for the purpose of human to make more verbose.
+
+echo Welcome To Shell Scripting
+echo "This is day 1 of shell scripting"
+echo Line1\n Line2
+echo Line 3
+echo Line 4
