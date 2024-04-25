ab343cc5b132b62968e78263542aab1d81737ba7
Hidayath <hidayath@aol.com>
2024-04-25 13:58:58 +0530

    update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo  " Configuring/$1.sh "
+echo  " Configuring frontend "
--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-bash components/frontend.sh
+bash components/$1.sh
+
+if [ $? -ne 0 ] ; then
+    echo -e "\e[31m examaple usage : bash wrapper.sh component name \e[0m"
+    exit 10
+fi
+
