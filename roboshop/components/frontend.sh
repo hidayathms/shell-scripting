de14dd5050a4ee958b4c934cfc831a0eb7f6c830
Hidayath <hidayath@aol.com>
2024-04-25 14:38:07 +0530

    update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,6 +6,7 @@ if [ $UID -ne 0 ] ; then
 echo " This script is expected to be executed with sudo or as a addministrator or root user"
 exit 1
 fi
+
 echo -e " \e[34m********Configuring frontend ******\e[0m"
 
 echo -e -n " \e[31m**Installing ngnix  **   : \e[0m"
@@ -17,11 +18,18 @@ else
     echo -e " \e[31m Failure \e[0m"
 fi
 
+echo -e " \e[34m********Dowonloading component $1 ******\e[0m"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo -e " \e[31m Failure \e[0m"
+fi
 
 
 # systemctl enable nginx
 # systemctl start nginx
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+# 
 # cd /usr/share/nginx/html
 # rm -rf *
 # unzip /tmp/frontend.zip
