880b74498056dd82daa7ee0315bd8cb74b8bdad2
Hidayath <hidayath@aol.com>
2024-04-25 13:46:27 +0530

    update

---
+++ 17-operators.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Expressions are catagorised in to 3 types and based on the expression we need to use operators
+<<COMMENT
+1. numbers
+2. strings
+3. files
+
+Operators on numbers
+-eq, -ne, -gt, -ge, -lt, -le
+[1 -eq 1]
+[1 -ne 1]
+
+Operator on strings
+= , ==, != 
+
+Operator on files:
+ [-f file] True of file exists and file is a regular file
+ [-d xyz ] True of file exists and it is a directory
+
+ COMMENT
+ 
