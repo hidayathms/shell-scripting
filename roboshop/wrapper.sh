d0fd85a5f270954dcf633ff1b68e5bb39f409a21
Hidayath <hidayath@aol.com>
2024-04-25 14:15:39 +0530

    update

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -2,8 +2,8 @@
 
 bash components/$1.sh
 
-if [ $? -ne 0 ] ; then
-    echo -e "\e[31m examaple usage : bash wrapper.sh component name \e[0m"
-    exit 10
-fi
+# if [ $? -ne 0 ] ; then
+#     echo -e "\e[31m examaple usage : bash wrapper.sh component name \e[0m"
+#     exit 10
+# fi
 
