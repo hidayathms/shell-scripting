f61a81d1e41ed3edc978b55dc49df69b66c2c99c
Hidayath <hidayath@aol.com>
2024-04-25 11:32:26 +0530

    update

---
+++ 14-ifelse.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+<<COMMENT
+if conditions is of three formats
+1. simple If
+2. If else
+3. Else If
+
+Simple If
+if [expression]; then
+commands
+if
+--commands to be executed only if the expression is true.
+COMMENT
+
+echo -e "Demon on If, Else and Else If Usage"
+
+ACTION=$1
+
+if [$ACTION=start]; then
+    echo "shippping starting"
+fi
