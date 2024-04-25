8edade14d205eebff41677b09ffa3e7d9563e4dc
Hidayath <hidayath@aol.com>
2024-04-25 11:17:25 +0530

    update

---
+++ 12-redirectors.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Redirectors are of 2 types in bash :
+
+# 1. input redirector     <  Ex. sudo sql <temp/sudentapp.sql
+# 2. Output redirector    > or >> or &>  
+# > - singe greater than symbol routes output to a file
+# >>- Double greater than symble add output to the top of the output file or append
+
+# OUtputs
+# 1) Standard Output      1> or > or >> 
+# 2) Standard error       2> or 2>>
+# 3) Standard output and standard error       &> or &>>
+
+
+ 
---
+++ 13-conditions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Conditions helps us to execute something only if some factor is true or false
+
+
+# Syntax of case
+# case  $var in 
+# opt1) command x ;;
+# opt2) command y ;;
+# esac
+
+# $1 REFERES FIRST COMMAND LINE ARGUMENT
+ACTION=$1
+
+case $ACTION in 
+    start)
+        echo -e " \e[34m Starting shipping servie \e[0m"
+        exit 0
+        ;;
+    stop) 
+        echo -e "\e[31m Stopping shipping service \e[0m"
+        exit 1
+        ;;
+    restart)
+        echo -e "\e[33m Restarting shipping service \e[0m"
+        exit 1
+        ;;
+    *)
+        echo -e "\e[35m Valid options are start, sotp or restart only \e[0m"
+        echo -e "\e[35m Example usage: \e[0m bash scriptname.sh start"
+esac
