0373146ab4266714b070479ce4e7857ee26eafa2
Hidayath <hidayath@aol.com>
2024-04-25 13:16:59 +0530

    update

---
+++ 16-loops.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Loops : when you want to run an action certain number of times we use loops.
+
+#There are 2 types of loops
+# 1) for loop - when you know somthing to be executed n number of times we use for loop
+# 2) while loop - When you dont know something to be executed n number of times we use while loops
+
+
+# for loop syntax
+
+echo value is 10
+echo value is 20
+echo value is 30
+echo value is 40
+echo value is 50
+echo value is 50
