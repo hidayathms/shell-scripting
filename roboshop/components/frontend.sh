54d3db4a52c964047c45cebc26b7724bb57b7d84
Hidayath <hidayath@aol.com>
2024-04-25 14:16:57 +0530

    update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,8 +3,10 @@
 echo -e " \e[34m********Configuring frontend ******\e[0m"
 
 
-echo -e " \e[31m********Installing ngnix  ******\e[0m"
+echo -e " \e[31m**Installing ngnix  **\e[0m"
 dnf install nginx -y 
+
+
 # systemctl enable nginx
 # systemctl start nginx
 # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
