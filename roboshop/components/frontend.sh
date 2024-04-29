46987760c25384291be3b46c72f8a3217051a11a
Hidayath <hidayath@aol.com>
2024-04-29 13:22:32 +0530

    mongo update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,69 +1,52 @@
 #!/bin/bash
 
-UID=$(id -u)
+USER_ID=$(id -u)
+COMPONENT=$1
+LOGFILE="/tmp/${COMPONENT}.logs
 
-if [ $UID -ne 0 ] ; then
+stat(){
+    if [ $1 -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo -e " \e[31m Failure \e[0m"
+fi
+}
+
+if [ $USER_ID -ne 0 ] ; then
 echo " This script is expected to be executed with sudo or as a addministrator or root user"
 exit 1
 fi
 
-echo -e " \e[34m********Configuring frontend ******\e[0m"
+echo -e " \e[34m********Configuring $COMPONENT ******\e[0m"
 
-echo -e -n "  Installing ngnix   : "
-dnf install nginx -y &>> /tmp/frontend.logs 
+echo -n "  Installing ngnix   : "
+dnf install nginx -y &>> $LOGFILE 
+stat $?
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e " \e[31m Failure \e[0m"
-fi
+echo -n "  Dowonloading component $COMPONENT : "
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
 
-echo -e -n "  Dowonloading component $1   : "
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e " \e[31m Failure \e[0m"
-fi
-
-echo -n "  Clean Up of $1 component :"
+echo -n "  Clean Up of $COMPONENT component :"
 cd /usr/share/nginx/html
-rm -rf *    &>> /tmp/frontend.logs
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e " \e[31m Failure \e[0m"
-fi
+rm -rf *    &>> $LOGFILE 
+stat $?
 
-echo -n "  Extract the $1 component :"
-unzip /tmp/frontend.zip     &>> /tmp/frontend.logs
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e " \e[31m Failure \e[0m"
-fi
+echo -n "  Extract the $COMPONENT component :"
+unzip /tmp/$COMPONENT.zip     &>> $LOGFILE 
+stat $?
 
-echo -n "  Configuring $1 component :"
-mv frontend-main/* .
+echo -n "  Configuring $COMPONENT component :"
+mv $COMPONENT-main/* .
 mv static/* .
-rm -rf frontend-main README.md
+rm -rf $COMPONENT-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e " \e[31m Failure \e[0m"
-fi
+stat $?
 
-echo -n "  Restarting $1 component :"
-systemctl enable nginx  &>> /tmp/frontend.logs
-systemctl daemon-reload &>> /tmp/frontend.logs
-systemctl restart nginx &>> /tmp/frontend.logs
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e " \e[31m Failure \e[0m"
-fi
+echo -n "  Restarting $COMPONENT component :"
+systemctl enable nginx  &>> $LOGFILE 
+systemctl daemon-reload &>> $LOGFILE 
+systemctl restart nginx &>> $LOGFILE 
+stat $?
 
-
-
-echo -e " \e[34m********$1 Component configuration completed ******\e[0m"
+echo -e " \e[34m********$COMPONENT Component configuration completed ******\e[0m"
