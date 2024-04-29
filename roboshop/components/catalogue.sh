eb7b7baf906e67a97e58daed09c49324caf3d289
Hidayath <hidayath@aol.com>
2024-04-29 15:32:37 +0530

    newupdate

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -53,7 +53,7 @@ unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE
 stat $?
 
 echo -n " Configuring $COMPONENT permissions : "
-mv /home/$APPUSER/${COMPONENT}-main $APPUSER_HOME
+mv {$APPUSER_HOME}-main $APPUSER_HOME
 chown -R $APPUSER:$APPUSER $APPUSER_HOME
 chmod -R 770 $APPUSER_HOME
 stat $?
