b44e40c0a9a410eb62b85f79d119b27217ae198f
Hidayath <hidayath@aol.com>
2024-05-01 15:02:00 +0530

    newupdate

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 COMPONENT=user
 
 source components/common.sh
@@ -8,10 +7,13 @@ source components/common.sh
 echo -e " \e[34m********Configuring $COMPONENT ******\e[0m"
 
 NODEJS          # Call nodejs function
+
 CREATE_USER
+
 DOWNLOAD_AND_EXTRACT
 
 CONFIG_SVC
 
 START_SVC
+
 echo -e " \e[34m********$COMPONENT Component configuration completed ******\e[0m"
