94ad911f32edc12af17063abbdda81527763a28c
Hidayath <hidayath@aol.com>
2024-05-01 14:58:52 +0530

    newupdate

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -44,10 +44,10 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
 echo -n "  Updating Revere proxy :"
-sed -i -e "/catalogue/s/localhost/catalogue.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
-# for component in catalogue user cart ; do
-# sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
-# done
+# sed -i -e "/catalogue/s/localhost/catalogue.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+for component in catalogue user cart ; do
+sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+done
 stat $?
 
 echo -n "  Restarting $COMPONENT component :"
