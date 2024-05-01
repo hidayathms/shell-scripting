67c9c6cd3ed0c500c358bf4519efcc26166d53cb
Hidayath <hidayath@aol.com>
2024-05-01 14:04:46 +0530

    newupdate

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -27,9 +27,10 @@ dnf install redis -y &>>LOGFILE
 stat $?
 
 echo -n " Update Config file in $COMPONENT   : "
-sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/redis.conf
-sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/redis/redis.conf
+sed -i -e "/bind/s/127.0.0.0/0.0.0.0/" /etc/redis.conf
+sed -i -e '/bind/s/127.0.0.0/0.0.0.0/' /etc/redis/redis.conf
 stat $?
+
 
 echo -n " Starting $COMPONENT  :"
 # systemctl enable redis  &>> $LOGFILE 
