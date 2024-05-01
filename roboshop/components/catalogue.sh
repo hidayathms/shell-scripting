fc86a1ad3858afab6b2d02858201e5b96a736950
Hidayath <hidayath@aol.com>
2024-05-01 11:54:15 +0530

    newupdate

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -5,62 +5,12 @@ COMPONENT=catalogue
 
 source components/common.sh
 
+NODEJS          # Call nodejs function
+CREATE_USER
+DOWNLOAD_AND_EXTRACT
 
-echo -e " \e[34m********Configuring $COMPONENT ******\e[0m"
+CONFIG_SVC
 
-echo -n " Disabling the default nodejs:10 repo and enabling nodejs18 on $COMPONENT : "
-dnf module disable nodejs -y &>> $LOGFILE
-dnf module enable nodejs:18 -y  &>> $LOGFILE
-stat $?
+START_SVC
 
-echo -n " Installing NodeJS : "
-dnf install nodejs -y   &>> $LOGFILE
-stat $?
-
-
-id $APPUSER &>>$LOGFILE
-if [ $? -ne 0 ] ; then
-echo -n " Creating $APPUSER : "
-useradd $APPUSER
-stat $?
-else 
-echo -n " User account exist : "
-echo -e " \e[32m Skipping \e[0m  : "
-fi
-
-echo -n " Downloading $COMPONENT : "
-curl -s -L -o /tmp/${COMPONENT}.zip $COMPONENT_URL
-stat $?
-
-echo -n " Extracting $COMPONENT : "
-cd /home/roboshop
-unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE
-stat $?
-
-echo -n " Performing Cleanup $COMPONENT : "
-rm -rf $APPUSER_HOME    &>> $LOGFILE
-stat $?
-
-echo -n " Configuring $COMPONENT permissions : "
-mv /home/$APPUSER/${COMPONENT}-main $APPUSER_HOME    &>> $LOGFILE
-chown -R $APPUSER:$APPUSER $APPUSER_HOME
-chmod -R 777 $APPUSER_HOME
-stat $?
-
-echo -n " Generating Artifacts : "
-cd $APPUSER_HOME
-npm install &>>$LOGFILE
-stat $?
-
-
-echo -n " Updading $COMPONENT Systemd file : "
-sudo sed -i -e 's/MONGO_DNSNAME/172.31.28.86/' ${APPUSER_HOME}/systemd.service
-mv ${APPUSER_HOME}/systemd.service /etc/systemd/system/${COMPONENT}.service
-stat $?
-
-echo -n " Starting $COMPONENT  :"
-systemctl daemon-reload &>> $LOGFILE 
-systemctl enable catalogue  &>> $LOGFILE 
-systemctl restart catalogue &>> $LOGFILE 
-stat $?
 echo -e " \e[34m********$COMPONENT Component configuration completed ******\e[0m"
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -17,3 +17,74 @@ if [ $USER_ID -ne 0 ] ; then
 echo " This script is expected to be executed with sudo or as a addministrator or root user"
 exit 1
 fi
+
+CREATE_USER(){
+
+id $APPUSER &>>$LOGFILE
+if [ $? -ne 0 ] ; then
+echo -n " Creating $APPUSER : "
+useradd $APPUSER
+stat $?
+else 
+echo -n " User account exist : "
+echo -e " \e[32m Skipping \e[0m  : "
+fi
+}
+
+NODEJS(){
+echo -n " Disabling the default nodejs:10 repo and enabling nodejs18 on $COMPONENT : "
+dnf module disable nodejs -y &>> $LOGFILE
+dnf module enable nodejs:18 -y  &>> $LOGFILE
+stat $?
+
+echo -n " Installing NodeJS : "
+dnf install nodejs -y   &>> $LOGFILE
+stat $?
+CREATE_USER
+START_SVC
+}
+
+DOWNLOAD_AND_EXTRACT(){
+  
+echo -n " Downloading $COMPONENT : "
+curl -s -L -o /tmp/${COMPONENT}.zip $COMPONENT_URL
+stat $?
+
+echo -n " Extracting $COMPONENT : "
+cd /home/roboshop
+unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE
+stat $?
+
+echo -n " Performing Cleanup $COMPONENT : "
+rm -rf $APPUSER_HOME    &>> $LOGFILE
+stat $?  
+}
+
+CONFIG_SVC(){
+    echo -n " Configuring $COMPONENT permissions : "
+    mv /home/$APPUSER/${COMPONENT}-main $APPUSER_HOME    &>> $LOGFILE
+    chown -R $APPUSER:$APPUSER $APPUSER_HOME
+    chmod -R 777 $APPUSER_HOME
+    stat $?
+    
+    echo -n " Generating Artifacts : "
+    cd $APPUSER_HOME
+    npm install &>>$LOGFILE
+    stat $?
+
+
+    echo -n " Updading $COMPONENT Systemd file : "
+    sudo sed -i -e 's/MONGO_DNSNAME/172.31.28.86/' -e 's/REDIS_ENDPOINT/172.31.17.183/' -e 's/CATALOGUE_ENDPOINT/172.31.20.135/' ${APPUSER_HOME}/systemd.service
+    mv ${APPUSER_HOME}/systemd.service /etc/systemd/system/${COMPONENT}.service
+    stat $?
+
+    
+}
+
+START_SVC(){
+    echo -n " Starting $COMPONENT  :"
+    systemctl daemon-reload &>> $LOGFILE 
+    systemctl enable $COMPONENT  &>> $LOGFILE 
+    systemctl restart $COMPONENT &>> $LOGFILE 
+stat $?
+}
