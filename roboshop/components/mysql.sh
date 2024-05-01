dc130b4dc959f1797c93bcb89709a4787f069312
Hidayath <hidayath@aol.com>
2024-05-01 20:09:23 +0530

    newupdate

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -32,8 +32,8 @@ stat $?
 fi 
 
 echo "show plugins;" | mysql -uroot -pRoboshop@1 | grep validate_password &>> $LOGFILE 
-if [ S? -eq 0 ] ; then
+if [ $? -eq 0 ] ; then
 echo -n "Uninstalling password-validate-plugin : "
-echo "uninstall plugin validate_password; " | mysql -uroot -pRoboshop@1 &>>$LOGFILE
+echo "uninstall plugin validate_password; " | mysql -uroot -pRoboshop@1 &>> $LOGFILE
 stat $?
 fi
