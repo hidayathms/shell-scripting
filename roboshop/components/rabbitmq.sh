3edc4e838043db384ba402c5e4bf4839e5da62cf
Hidayath <hidayath@aol.com>
2024-05-02 22:23:17 +0530

    newupdate

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -5,10 +5,10 @@ COMPONENT=rabbitmq
 source components/common.sh
 
 echo -e " \e[34m********Configuring $COMPONENT  dependencies ******\e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash  &>>LOGFILE
-stat $?
+
 
 echo -e " \e[34m********Configuring $COMPONENT  repo ******\e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash  &>>LOGFILE
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash  &>>LOGFILE
 stat $?
 
@@ -16,7 +16,10 @@ echo -n " Installing $COMPONENT   : "
 dnf install rabbitmq-server -y &>>LOGFILE
 stat $?
 
-START_SVC
+echo -n " Starting $COMPONENT  :"
+systemctl enable rabbitmq-server 
+systemctl start rabbitmq-server
+systemctl status rabbitmq-server -l
 
 echo -n " Creating $APPUSER for $COMPONENT: "
 rabbitmqctl add_user roboshop roboshop123
