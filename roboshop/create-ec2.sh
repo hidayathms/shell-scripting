f10b49d85df83fef3ef47d2f91242bffd6d34087
Hidayath <hidayath@aol.com>
2024-05-04 17:25:43 +0530

    newupdate

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -17,14 +17,24 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Cen
 SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-allow-firewall" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
 INSTANCE_TYPE="t3.micro"
 
-
+create_server(){
 echo -n -e " ****$COMPONENT Server in Progress *****!!!!"
 # aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" &> $LOGFILE
 PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq ".Instance[].PrivateIpAddress" | sed -e 's/"//g' ) &>$LOGFILE
 
 
-echo -n -e " ****$COMPONENT DNS Record creation in Progress *****!!!! :"
+echo -e " ****$COMPONENT DNS Record creation in Progress *****!!!! :"
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json 
 
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json
 
+}
+
+if [ "$1" == "all" ]; then
+    for component in frontend mongodb catalogue redis user cart mysql shipping reabbitmq payment; do
+        COMPONENT=$component
+        create_server
+    done
+else 
+    create_server
+fi
