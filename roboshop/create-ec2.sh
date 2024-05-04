11a842e6a9f7fb826ea977f945e6b04fcadd66bc
Hidayath <hidayath@aol.com>
2024-05-04 17:11:23 +0530

    newupdate

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -5,7 +5,11 @@
 
 # AMI_ID="ami-072983368f2a6eab5" Not the correct approach
 # SecurityGroup_ID="sg-014143c52beef6877"
-source components/common.sh
+
+if [ -z $1 ] ; then
+echo -e " Compnent name is needed "
+exit 1
+fi
 
 COMPONENT=$1
 HOSTEDZONEID="Z01822012WSURFZZ552LY"
@@ -17,9 +21,10 @@ INSTANCE_TYPE="t3.micro"
 echo -n -e " ****$COMPONENT Server in Progress *****!!!!"
 # aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" &> $LOGFILE
 PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq ".Instance[].PrivateIpAddress" | sed -e 's/"//g' ) &>$LOGFILE
-stat $?
+
 
 echo -n -e " ****$COMPONENT DNS Record creation in Progress *****!!!! :"
-sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json
+sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json 
+
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json
 
