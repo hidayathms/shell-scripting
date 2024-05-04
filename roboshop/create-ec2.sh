1273d7618f91ee7db07ee120025c13c30bda7631
Hidayath <hidayath@aol.com>
2024-05-04 15:43:48 +0530

    newupdate

---
+++ roboshop/create-ec2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script is going to create EC2 servers
+
+
+# AMI_ID="ami-072983368f2a6eab5" Not the correct approach
+# SecurityGroup_ID="sg-014143c52beef6877"
+
+COMPONENT=$1
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Centos-8" | jq ".Images[].ImageId" | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-allow-firewall" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
+INSTANCE_TYPE="t3.micro"
+
+
+echo -e " ****Creating Server in Progress *****!!!!"
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
