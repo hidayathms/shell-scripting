45c917af6399c8e55d70f77f31c6bbf0498776c5
Hidayath <hidayath@aol.com>
2024-05-04 15:46:55 +0530

    newupdate

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -5,6 +5,7 @@
 
 # AMI_ID="ami-072983368f2a6eab5" Not the correct approach
 # SecurityGroup_ID="sg-014143c52beef6877"
+source components/common.sh
 
 COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Centos-8" | jq ".Images[].ImageId" | sed -e 's/"//g')
@@ -13,4 +14,5 @@ INSTANCE_TYPE="t3.micro"
 
 
 echo -e " ****Creating Server in Progress *****!!!!"
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" &> $LOGFILE
+stat $?
